ea71eb28223c1633b6e4447230da15043b9086ad
x2z54 <flashbulb54@gmail.com>
2013-03-27 00:17:21 +0400

    new app

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,99 +1,41 @@
-# По умолчанию для дистрибуции проектов используется Bundler.
-# Эта строка включает автоматическое обновление и установку
-# недостающих gems, указанных в вашем Gemfile.
-#
-## !!! Не забудьте добавить
-# gem 'capistrano'
-# gem 'unicorn'
-#
-# в ваш Gemfile.
-#
-# Если вы используете другую систему управления зависимостями,
-# закомментируйте эту строку.
-require 'bundler/capistrano'
+set :application, "manvel"
+set :repository,  "file:///home/andrew/Dropbox/Rails/shop"
 
-## Чтобы не хранить database.yml в системе контроля версий, поместите
-## dayabase.yml в shared-каталог проекта на сервере и раскомментируйте
-## следующие строки.
+# set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
+# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
-# after "deploy:update_code", :copy_database_config
-# task :copy_database_config, roles => :app do
-#   db_config = "#{shared_path}/database.yml"
-#   run "cp #{db_config} #{release_path}/config/database.yml"
-# end
+set :deploy_via, :copy
 
-# В rails 3 по умолчанию включена функция assets pipelining,
-# которая позволяет значительно уменьшить размер статических
-# файлов css и js.
-# Эта строка автоматически запускает процесс подготовки
-# сжатых файлов статики при деплое.
-# Если вы не используете assets pipelining в своем проекте,
-# или у вас старая версия rails, закомментируйте эту строку.
-load 'deploy/assets'
+dpath = "/home/hosting_x2z54/projects/manvel"
 
-# Для удобства работы мы рекомендуем вам настроить авторизацию
-# SSH по ключу. При работе capistrano будет использоваться
-# ssh-agent, который предоставляет возможность пробрасывать
-# авторизацию на другие хосты.
-# Если вы не используете авторизацию SSH по ключам И ssh-agent,
-# закомментируйте эту опцию.
-ssh_options[:forward_agent] = true
+set :user, "hosting_x2z54"
+set :use_sudo, false
+set :deploy_to, dpath
 
-# Имя вашего проекта в панели управления.
-# Не меняйте это значение без необходимости, оно используется дальше.
-set :application,     "manvel"
+set :scm, :git
 
-# Сервер размещения проекта.
-set :deploy_server,   "fluorine.locum.ru"
 
-# Не включать в поставку разработческие инструменты и пакеты тестирования.
-set :bundle_without,  [:development, :test]
+role :web, "fluorine.locum.ru"                          # Your HTTP server, Apache/etc
+role :app, "fluorine.locum.ru"                          # This may be the same as your `Web` server
+role :db,  "fluorine.locum.ru", :primary => true # This is where Rails migrations will run
+run
 
-set :user,            "hosting_x2z54"
-set :login,           "x2z54"
-set :use_sudo,        false
-set :deploy_to,       "/home/#{user}/projects/#{application}"
-set :unicorn_conf,    "/etc/unicorn/#{application}.#{login}.rb"
-set :unicorn_pid,     "/var/run/unicorn/#{application}.#{login}.pid"
-set :bundle_dir,      File.join(fetch(:shared_path), 'gems')
-role :web,            deploy_server
-role :app,            deploy_server
-role :db,             deploy_server, :primary => true
+after "deploy:update_code", :copy_database_config
 
-# Следующие строки необходимы, т.к. ваш проект использует rvm.
-set :rvm_ruby_string, "1.9.3"
-set :rake,            "rvm use #{rvm_ruby_string} do bundle exec rake" 
-set :bundle_cmd,      "rvm use #{rvm_ruby_string} do bundle"
-
-# Настройка системы контроля версий и репозитария,
-# по умолчанию - git, если используется иная система версий,
-# нужно изменить значение scm.
-set :scm,             :git
-
-# Предполагается, что вы размещаете репозиторий Git в вашем
-# домашнем каталоге в подкаталоге git/<имя проекта>.git.
-# Подробнее о создании репозитория читайте в нашем блоге
-# http://locum.ru/blog/hosting/git-on-locum
-set :repository,      "ssh://#{user}@#{deploy_server}/home/#{user}/git/#{application}.git"
-
-## Если ваш репозиторий в GitHub, используйте такую конфигурацию
-# set :repository,    "git@github.com:username/project.git"
-
-## --- Ниже этого места ничего менять скорее всего не нужно ---
-
-before 'deploy:finalize_update', 'set_current_release'
-task :set_current_release, :roles => :app do
-    set :current_release, latest_release
+task :copy_database_config, roles => :app do
+  db_config = "#{shared_path}/database.yml"
+  run "cp #{db_config} #{release_path}/config/database.yml"
 end
 
-  set :unicorn_start_cmd, "(cd #{deploy_to}/current; rvm use #{rvm_ruby_string} do bundle exec unicorn_rails -Dc #{unicorn_conf})"
-
+set :unicorn_rails, "/var/lib/gems/1.8/bin/unicorn_rails"
+set :unicorn_conf, "/etc/unicorn/manvel.x2z54.rb"
+set :unicorn_pid, "/var/run/unicorn/manvel.x2z54.pid"
 
 # - for unicorn - #
 namespace :deploy do
   desc "Start application"
   task :start, :roles => :app do
-    run unicorn_start_cmd
+    run "#{unicorn_rails} -Dc #{unicorn_conf}"
   end
 
   desc "Stop application"
@@ -103,6 +45,22 @@ namespace :deploy do
 
   desc "Restart Application"
   task :restart, :roles => :app do
-    run "[ -f #{unicorn_pid} ] && kill -USR2 `cat #{unicorn_pid}` || #{unicorn_start_cmd}"
+    run "[ -f #{unicorn_pid} ] && kill -USR2 `cat #{unicorn_pid}` || #{unicorn_rails} -Dc #{unicorn_conf}"
   end
 end
+
+
+# if you want to clean up old releases on each deploy uncomment this:
+# after "deploy:restart", "deploy:cleanup"
+
+# if you're still using the script/reaper helper you will need
+# these http://github.com/rails/irs_process_scripts
+
+# If you are using Passenger mod_rails uncomment this:
+# namespace :deploy do
+#   task :start do ; end
+#   task :stop do ; end
+#   task :restart, :roles => :app, :except => { :no_release => true } do
+#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+#   end
+# end
