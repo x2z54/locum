7c97b4a32d1ae2147e54458d9a38b4de94533eed
Andrew <flashbulb54@gmail.com>
2013-04-24 17:44:10 +0400

    span

--- db/schema.rb
+++ db/schema.rb
@@ -26,11 +26,6 @@ ActiveRecord::Schema.define(:version => 20130330143555) do
     t.datetime "updated_at",  :null => false
   end
 
-  create_table "cart_items", :force => true do |t|
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
-
   create_table "carts", :force => true do |t|
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
